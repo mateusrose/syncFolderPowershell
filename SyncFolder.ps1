e7878a94eaeccbd4287bdeaee0a85d5e98e61ba5
mateusrose <pedrompmrosa@gmail.com>
2024-07-10 12:41:28 +0100

    simplifiedlogfilecall

--- SyncFolder.ps1
+++ SyncFolder.ps1
@@ -11,20 +11,7 @@
 
 
 #function that logs changes into a file and console, takes 2 parameters
-function Log-Write {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Message,
 
-        [Parameter(Mandatory)]
-        [string]$LogFilePath
-    )
-
-    #to dont repeat date every time when calling
-    $formattedMessage = "[{0}] {1}" -f (Get-Date), $Message
-    Write-Host $formattedMessage
-    Add-Content -Path $LogFilePath -Value $formattedMessage
-}
 
 #function that holds the sync folder logic
 function Sync-Folder {
@@ -33,11 +20,23 @@ function Sync-Folder {
         [string]$SourceFolder,
 
         [Parameter(Mandatory)]
-        [string]$ReplicaFolder,
-
-        [Parameter(Mandatory)]
-        [string]$LogFilePath
+        [string]$ReplicaFolder
     )
+
+    function Log-Write {
+        param (
+            [Parameter(Mandatory)]
+            [string]$Message 
+        )
+
+        #to dont repeat date every time when calling
+        $formattedMessage = "[{0}] {1}" -f (Get-Date), $Message
+        Write-Host $formattedMessage
+        Add-Content -Path $Script:logFile -Value $formattedMessage
+}
+
+
+
 
     #possible errors
     try {
@@ -49,7 +48,7 @@ function Sync-Folder {
         #check if Replica Folder exists, create if not
         if (-not (Test-Path -Path $ReplicaFolder -PathType Container)) {
             New-Item -ItemType Directory -Path $ReplicaFolder | Out-Null
-            Log-Write -Message "Created replica folder: $ReplicaFolder" -LogFilePath $LogFilePath
+            Log-Write -Message "Created replica folder: $ReplicaFolder" 
         }
 
         #sync new files from source to replica
@@ -60,12 +59,12 @@ function Sync-Folder {
             #checking if its a folder and doesnt exist in replica folder
             if ($_.PsIsContainer -and -not (Test-Path -Path $replicaPath -PathType Container)) {
                 New-Item -ItemType Directory -Path $replicaPath | Out-Null
-                Log-Write -Message "Created directory: $replicaPath" -LogFilePath $LogFilePath
+                Log-Write -Message "Created directory: $replicaPath" 
 
             #other file types if not present in replica folder or last write time of source file is newer than replica file
             } elseif (-not (Test-Path -Path $replicaPath) -or $_.LastWriteTime -gt (Get-Item -Path $replicaPath).LastWriteTime) {
                 Copy-Item -Path $sourcePath -Destination $replicaPath -Force
-                Log-Write -Message "Copied file: $sourcePath to $replicaPath" -LogFilePath $LogFilePath
+                Log-Write -Message "Copied file: $sourcePath to $replicaPath" 
             }
         }
 
@@ -80,11 +79,11 @@ function Sync-Folder {
                 #check if folder
                 if ($_.PsIsContainer) {
                     Remove-Item -Path $_.FullName -Recurse -Force
-                    Log-Write -Message "Removed directory: $($_.FullName)" -LogFilePath $LogFilePath
+                    Log-Write -Message "Removed directory: $($_.FullName)" 
                 #other files
                 } else {
                     Remove-Item -Path $_.FullName -Force
-                    Log-Write -Message "Removed file: $($_.FullName)" -LogFilePath $LogFilePath
+                    Log-Write -Message "Removed file: $($_.FullName)"
                 }
             }
         }
@@ -100,7 +99,7 @@ $logFile = Join-Path -Path $logFilePath -ChildPath "syncLog.log"
 
 try {
     #passing arguments from script to Sync-Folder function, passing logFile that is the combination of all add-content operations of the log file
-    Sync-Folder -SourceFolder $SourceFolder -ReplicaFolder $ReplicaFolder -LogFilePath $logFile
+    Sync-Folder -SourceFolder $SourceFolder -ReplicaFolder $ReplicaFolder
 } catch {
     Write-Host "Error: $_" 
 }
